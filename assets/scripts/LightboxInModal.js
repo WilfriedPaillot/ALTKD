10be6305895af495f151d119a7cbd7e5d1af635a
WilfriedPaillot <paillotwilfried@gmail.com>
2021-08-01 03:29:04 +0200

    Add some style on modal

--- assets/scripts/LightboxInModal.js
+++ assets/scripts/LightboxInModal.js
@@ -1,6 +1,6 @@
 document.addEventListener("click",function (e){
     //console.log(e.target);
-    if (e.target.classList.contains("gallerie-img")){
+    if (e.target.classList.contains("img-thumbnail")){
         const src = e.target.getAttribute("src");
         const paragraphContent = e.target.getAttribute("alt");
         document.querySelector(".modal-img").src = src;

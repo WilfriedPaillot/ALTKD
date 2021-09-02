996b8c7c8f340b5ea937e677eee5b208fb081d23
WilfriedPaillot <paillotwilfried@gmail.com>
2021-09-02 02:49:37 +0200

    change script + use bundle copy in assets

--- assets/scripts/LightboxInModal.js
+++ assets/scripts/LightboxInModal.js
@@ -1,4 +1,3 @@
-const bootstrapBundle = require("bootstrap/dist/js/bootstrap.bundle.js");
 document.addEventListener("click",function (e){
     //console.log(e.target);
     if (e.target.classList.contains("img-thumbnail")){
@@ -6,7 +5,7 @@ document.addEventListener("click",function (e){
         const paragraphContent = e.target.getAttribute("alt");
         document.querySelector(".modal-img").src = src;
         document.querySelector(".modal-img-legend").textContent = paragraphContent;
-        const myModal = new bootstrapBundle.Modal(document.getElementById('gallerie-modal'));
-        myModal.show();
+        const myModal = document.getElementById('gallerie-modal');
+        myModal.show(myModal);
     }
 })

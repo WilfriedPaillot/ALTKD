12567c37a2340791e020972e3593ce57618c4bb3
WilfriedPaillot <paillotwilfried@gmail.com>
2021-09-04 16:37:55 +0200

    Fix somes bugs on paysage + update bk in onScroll

--- assets/scripts/OnScroll.js
+++ assets/scripts/OnScroll.js
@@ -3,14 +3,14 @@ window.onscroll = function () {
         document.getElementById("navigation").classList.remove("bg-transparent");
         document.getElementById("navigation").classList.add("fixed-top", "bg-eerie-black");
         document.getElementById("logo").classList.remove("d-none");
-        document.getElementById("vertical_title").classList.remove("d-lg-inline-flex");
+        document.getElementById("vertical_title").classList.remove("d-xl-inline-flex");
     }
     else { 
         if(document.documentElement.scrollTop < 50){
         document.getElementById("navigation").classList.remove("fixed-top", "bg-eerie-black");
         document.getElementById("navigation").classList.add("bg-transparent");
         document.getElementById("logo").classList.add("d-none");
-        document.getElementById("vertical_title").classList.add("d-lg-inline-flex");
+        document.getElementById("vertical_title").classList.add("d-xl-inline-flex");
 
         }
     }
